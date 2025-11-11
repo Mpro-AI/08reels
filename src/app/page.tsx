7724a1062243c4b204c6e4d16cffd0af5a336fa5
Mance Li <manceli@m-pro.com.hk>
2025-11-11 17:36:53 +0000

    取消他的載入狀態

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,19 +14,7 @@ export default function DashboardPage() {
       <div className="flex flex-1 flex-col">
         <Header title="專案影片" />
         <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
-          {loading ? (
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              {Array.from({ length: 4 }).map((_, i) => (
-                <div key={i} className="space-y-2">
-                  <Skeleton className="h-40 w-full" />
-                  <Skeleton className="h-6 w-3/4" />
-                  <Skeleton className="h-4 w-1/2" />
-                </div>
-              ))}
-            </div>
-          ) : (
-            <VideoGrid videos={videos ?? []} />
-          )}
+          <VideoGrid videos={videos ?? []} />
         </main>
       </div>
     </AppLayout>
